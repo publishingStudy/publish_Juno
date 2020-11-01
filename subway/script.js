bf499a876f7c951764facb2111749192fa2bab5d
junholee <juno7803@naver.com>
2020-11-02 03:44:09 +0900

    feat: main-menu 배치 수정 및 이벤트 배너 추가

---
+++ subway/script.js
@@ -0,0 +1,11 @@
+// const header = document.querySelector('.header');
+// const subMenu = document.querySelectorAll('.sub-menu');
+
+// header.addEventListener('mouseover',(e)=>{
+//     e.target.style.overflow = "inherit";
+//     subMenu.opacity = 1;
+// })
+// header.addEventListener('mouseout',(e)=>{
+//     e.target.style.overflow = "hidden";
+//     subMenu.opacity = 0 ;
+// })
